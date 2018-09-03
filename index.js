573c858fd7e950342c232fec41034c4f4188dd45
Julian Brown <julianxbrown@gmail.com>
2018-09-03 14:06:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // write your code below!
+//function named happyHoliday no parameters return 'Happy holidays!'
+function happyHolidays() {
+  return 'Happy holidays!';
+}
+//
+function happyHolidaysTo(name) {
+  return 'Happy holidays, ${name}!'
+}
+
